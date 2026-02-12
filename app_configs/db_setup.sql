4a8a7824b90e56918f2766ccfd6540b5303e3686
Davlit-ops <mozilla.davlit@gmail.com>
2026-02-12 12:09:57 +0000

    feat: add DB initialization and systemd service configs

---
+++ app_configs/db_setup.sql
@@ -0,0 +1,13 @@
+-- Create the database
+CREATE DATABASE eschool;
+
+-- Create a user and allow connections from any IP (controlled by Security Groups)
+-- Use a strong password as per requirements
+CREATE USER 'Your_user'@'%' IDENTIFIED BY 'YourPassword';
+
+-- Grant all privileges on the new database to the user
+GRANT ALL PRIVILEGES ON eschool.* TO 'Your_user'@'%';
+
+-- Apply changes
+FLUSH PRIVILEGES;
+EXIT;
